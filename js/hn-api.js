bffe985cab9b24137667f7862fe91688dd3e7445
stizzle123 <arifayanidowu2@yahoo.com>
2019-01-10 13:02:24 +0100

    Sticky footer

--- js/hn-api.js
+++ js/hn-api.js
@@ -5,27 +5,29 @@ fetch("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty")
   .then(res => res.json())
   .then(data => {
     let results = data.slice(0, 10);
-    results.forEach((element, index) => {
-      fetch(
-        `https://hacker-news.firebaseio.com/v0/item/${element}.json?print=pretty`
-      )
-        .then(res => res.json())
-        .then(data => {
-          output += `
-            <ul class="card mt-1">
+    results
+      .sort((a, b) => a - b)
+      .forEach((element, index) => {
+        fetch(
+          `https://hacker-news.firebaseio.com/v0/item/${element}.json?print=pretty`
+        )
+          .then(res => res.json())
+          .then(data => {
+            output += `
+            <ul class="card">
               <li><strong>${index}</strong>. ${data.title}</li>
               <li>
-              <small>
-              ${data.score} by <em>${data.by}</em> &mdash; ${new Date(
-            data.time
-          ).getHours()} hours ago | ${data.descendants} comments
+              <small class="text-gray">
+              ${data.score} points by <em>${data.by}</em> &mdash; ${new Date(
+              data.time
+            ).getMinutes()} minutes ago | ${data.descendants} comments
           </small>
           </li>
             </ul>
           `;
-          template.innerHTML = output;
-        })
-        .catch(err => console.log(err));
-    });
+            template.innerHTML = output;
+          })
+          .catch(err => console.log(err));
+      });
   })
   .catch(err => console.log(err));
