715d4f8482405f0e6ac02338e356cf47492d70a4
stizzle123 <arifayanidowu2@yahoo.com>
2019-01-10 11:19:53 +0100

    Displaying data on the UI

--- js/hn-api.js
+++ js/hn-api.js
@@ -1,7 +1,31 @@
-let data = "";
-let lists = [];
+let output = "";
+const template = document.querySelector("#app");
 
-fetch("https://hacker-news.firebaseio.com/v0/item/8863.json?print=pretty")
+fetch("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty")
   .then(res => res.json())
-  .then(data => console.log(data))
+  .then(data => {
+    let results = data.slice(0, 10);
+    results.forEach((element, index) => {
+      fetch(
+        `https://hacker-news.firebaseio.com/v0/item/${element}.json?print=pretty`
+      )
+        .then(res => res.json())
+        .then(data => {
+          output += `
+            <ul class="card mt-1">
+              <li><strong>${index}</strong>. ${data.title}</li>
+              <li>
+              <small>
+              ${data.score} by <em>${data.by}</em> &mdash; ${new Date(
+            data.time
+          ).getHours()} hours ago | ${data.descendants} comments
+          </small>
+          </li>
+            </ul>
+          `;
+          template.innerHTML = output;
+        })
+        .catch(err => console.log(err));
+    });
+  })
   .catch(err => console.log(err));
